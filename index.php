1405197d466e99d5a62b3e667f2bb189e9d99a68
2516e <mahleb.enzo@gmail.com>
2024-06-19 16:49:44 +0200

    19.06.24

---
+++ index.php
@@ -0,0 +1,78 @@
+    <?php require "public/header.html.php" ?>
+    <main>
+        <!-- section.row>div.col{$}*12 -->
+        <!-- ajouter class border -->
+        <section class="row">
+            <div class="col border">1</div>
+            <div class="col border">2</div>
+            <div class="col border">3</div>
+            <div class="col border">4</div>
+            <div class="col border">5</div>
+            <div class="col border">6</div>
+            <div class="col border">7</div>
+            <div class="col border">8</div>
+            <div class="col border">9</div>
+            <div class="col border">10</div>
+            <div class="col border">11</div>
+            <div class="col border">12</div>
+        </section>
+        <!-- section2 avec 2 col  -->
+        <section class="row">
+            <div class="col border">1</div>
+            <div class="col border">2</div>
+        </section>
+        <!-- section3 avec col-4 col-8  -->
+        <section class="row">
+            <div class="col-4 border">col-4</div>
+            <div class="col-8 border">col-8</div>
+        </section>
+        <!-- section4 dépasser 12 col  -->
+        <section class="row">
+            <div class="col border">1</div>
+            <div class="col border">2</div>
+            <div class="col border">3</div>
+            <div class="col border">4</div>
+            <div class="col border">5</div>
+            <div class="col border">6</div>
+            <div class="col border">7</div>
+            <div class="col border">8</div>
+            <div class="col border">9</div>
+            <div class="col border">10</div>
+            <div class="col border">11</div>
+            <div class="col border">12</div>
+            <div class="col border">13</div>
+            <div class="col border">14</div>
+        </section>
+        <!-- section5 3 col avec un titre et un texte  -->
+        <section class="row">
+            <div class="col border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+            <div class="col border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+            <div class="col border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+        </section>
+        <!-- section6 3 col-sm avec un titre et un texte  -->
+        <section class="row">
+            <div class="col-sm border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+            <div class="col-sm border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+            <div class="col-sm border">
+                <h1>Titre</h1>
+                <p>texte</p>
+            </div>
+        </section>
+
+    </main>
+    <?php require "public/footer.html.php" ?>
